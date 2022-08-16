542e61b6433afda94aba098a6033937a48726870
m-a-ulfat <111355959+m-a-ulfat@users.noreply.github.com>
2022-08-16 08:22:15 -0300

    Add files via upload

---
+++ css/Rana.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class A{
+    Public:
+    int x,y;
+    public:
+    A()
+    {
+        x=y=100;
+    }
+
+    void show()
+    {
+        cout<<"The value of X is :"<<x<<endl;
+        cout<<"The value of Y is :"<<x<<endl;
+        cout<<"The Average of X and Y is :"<<(x+y)2<<endl;
+    }
+};
+int main()
+{
+A a;
+a.show();
+}
